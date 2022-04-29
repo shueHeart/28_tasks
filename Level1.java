9fe4b83e4436a1116727820ca33e29b7fa7b2b1f
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-04-29 23:57:21 +0400

    Update Level1.java
    edit

--- Level1.java
+++ Level1.java
@@ -70,24 +70,24 @@ public class Level1
 //     }
     //new
      public static int ConquestCampaign(int N, int M, int L, int [] battalion) {
-        int[][] plac = new int[M][N];
-        for (int x = 0; x < M; ++x) {
-            for (int y = 0; y < N; ++y) {
+        int[][] plac = new int[N][M];
+        for (int x = 0; x < N; ++x) {
+            for (int y = 0; y < M; ++y) {
                 plac[x][y] = 0;
             }
         }
         for (int i = 0; i < L; ++i) {
-            plac[battalion[i + 1]][battalion[i]] = 1;
+            plac[battalion[i] - 1][battalion[i + 1] - 1] = 1;
         }
         int days = 1;
         while (notConquest(N, M, plac)){
             List<int[]> list = new ArrayList<int[]>();
-            for (int x = 0; x < M; ++x) {
-                for (int y = 0; y < N; ++y) {
+            for (int x = 0; x < N; ++x) {
+                for (int y = 0; y < M; ++y) {
                     if (plac[x][y] != 0) {
-                        if(x + 1 < M) list.add(new int[]{x + 1, y});
+                        if(x + 1 < N) list.add(new int[]{x + 1, y});
                         if(x - 1 > -1) list.add(new int[]{x - 1, y});
-                        if(y + 1 < N) list.add(new int[]{x, y + 1});
+                        if(y + 1 < M) list.add(new int[]{x, y + 1});
                         if(y - 1 > -1) list.add(new int[]{x, y - 1});
                     }
                 }
@@ -103,8 +103,8 @@ public class Level1
         return days;
     }
     private static boolean notConquest(int N, int M, int[][] plac) {
-        for (int x = 0; x < M; ++x) {
-            for (int y = 0; y < N; ++y) {
+        for (int x = 0; x < N; ++x) {
+            for (int y = 0; y < M; ++y) {
                 if(plac[x][y] == 0){
                     return true;
                 }
