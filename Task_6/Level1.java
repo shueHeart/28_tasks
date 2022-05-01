172d822deac5a8dccc9c855cb7a3084ded4a654f
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-05-01 14:28:44 +0400

    Update Level1.java
    fix task 6

--- Task_6/Level1.java
+++ Task_6/Level1.java
@@ -18,8 +18,8 @@ public class Level1
 		
 		double sumDistance = 0;
 		for (int i = 0; i < N - 1; ++i) {
-			double cat1 = Math.abs(pointCoordinates[hits[i] - 1][0] - pointCoordinates[hits[i]][0]);
-			double cat2 = Math.abs(pointCoordinates[hits[i] - 1][1] - pointCoordinates[hits[i]][1]);
+			double cat1 = Math.abs(pointCoordinates[hits[i] - 1][0] - pointCoordinates[hits[i + 1] - 1][0]);
+			double cat2 = Math.abs(pointCoordinates[hits[i] - 1][1] - pointCoordinates[hits[i + 1] - 1][1]);
 			double distance = Math.sqrt(Math.pow(cat1, 2) + Math.pow(cat2, 2));
 			sumDistance += distance;
 		}
