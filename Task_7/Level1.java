e3ca272a1854fc01f0047a942b49fecf4f86045b
shueHeart <levgromov667@gmail.com>
2022-05-02 15:05:23 +0400

    task 7 completed

---
+++ Task_7/Level1.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class Level1
+{
+	public static int [] WordSearch (int len, String s, String subs) {
+		List<String> extractedStrings = new ArrayList<String>();
+		s = s.trim();
+		while (s.length() != 0) {
+			String extractedString = "";
+			while (extractedString.length() < len) {
+				int firstSpaceIndex = s.indexOf(" ");
+				String space = "";
+				if (firstSpaceIndex != -1) {
+					space = s.substring(0, firstSpaceIndex);
+					if(space.length() + extractedString.length() + 1 <= len ) {
+						s = s.replaceFirst(space + " ", "");
+						if (extractedString.length() != 0) {
+							extractedString += " " + space;
+						} else {
+							extractedString += space;
+						}
+					}else if(space.length() + extractedString.length() + 1 > len) {
+						if (extractedString.length() == 0) {
+							space = s.substring(0, len);
+							s = s.replaceFirst(space, "");
+							extractedString += space;
+							break;
+						}
+							
+						break;
+					}
+				} else {
+					if (extractedString.length() == 0) {
+						if (s.length() <= len) {
+							space = s.substring(0, s.length());
+							s = s.replaceFirst(space, "");
+							extractedString += space;
+						} else {
+							space = s.substring(0, len);
+							s = s.replaceFirst(space, "");
+							extractedString += space;
+						}
+					}
+					break;
+				}
+					
+
+			}
+			extractedString = extractedString.trim();
+			System.out.println(extractedString);
+			if(extractedString != " ") extractedStrings.add(extractedString);			
+		}
+		int[] yesOrNot = new int[extractedStrings.size()];
+		for(int i = 0; i < extractedStrings.size(); ++i) {
+			String extracted = extractedStrings.get(i);
+			yesOrNot[i] = 0;
+			while(extracted.length() != 0) {
+				int index = extracted.indexOf(" ");
+				if (index != -1) {
+					String checking = extracted.substring(0, index);
+					if (checking.equals(subs)) {
+						yesOrNot[i] = 1;
+					}
+					extracted = extracted.replaceFirst(checking + " ", "");
+				} else {
+					if(extracted.equals(subs)) {
+						yesOrNot[i] = 1;
+					}
+					extracted = extracted.replaceFirst(extracted, "");
+					break;
+				}
+			}
+		}
+		return yesOrNot;
+	}
+
+}
