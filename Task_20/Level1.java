cf0c2ab6f0f33fae683e6c4cefbbdaa7502fd95c
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-05-26 00:18:54 +0400

    Update Level1.java
    rework

--- Task_20/Level1.java
+++ Task_20/Level1.java
@@ -5,21 +5,28 @@ public class Level1
 	
 	private static String text = "";
 	
-	private static String completedCommands = "";
+	private static String[] history = new String[0];
 	
+	private static int historyIndex = -1;
 	
 	public static String BastShoe(String command) {
 		
 		char numOperation = command.charAt(0);
+//		System.out.println("****");
+//		for (int i = 0; i < history.length; ++i) {
+//			System.out.println(history[i]);
+//		}
+//		System.out.println("****");
+		
 		
 		switch (numOperation) {
 			case '1' : {
-				add(command.substring(2));
+				add(command.substring(2), false);
 				return text;
 			}
 			
 			case '2' : {
-				delete(Integer.parseInt(command.substring(2)));
+				delete(Integer.parseInt(command.substring(2)), false);
 				return text;
 			}
 			
@@ -28,11 +35,13 @@ public class Level1
 			}
 			
 			case '4' : {
-				
+				undo();
+				return text;
 			}
 			
 			case '5' : {
-				
+				redo();
+				return text;
 			}
 		}
 		return "here";
@@ -40,12 +49,23 @@ public class Level1
 	}
 	
 	
-	private static void add (String S) {
+	private static void add (String S, boolean onHistory) {
 		text += S;
-		completedCommands += "1 " + S + " ";
+		
+		if (!onHistory && historyIndex == history.length - 1) {
+			addNewAction(1, S);
+			return;
+		}
+		else if (onHistory) {
+			return;
+		}
+		
+		history = new  String[0];
+		addNewAction(1, S);
+		
 	}
 	
-	private static void delete (int N) {
+	private static void delete (int N, boolean onHistory) {
 		if (N > text.length()) {
 			text = "";
 		}
@@ -59,7 +79,17 @@ public class Level1
 			
 			text = new StringBuilder(reversed).reverse().toString();
 			
-			completedCommands += "2 " + new StringBuilder(forDelete).reverse().toString() + " ";
+			if (!onHistory && historyIndex == history.length - 1) {
+				addNewAction(2, new StringBuilder(forDelete).reverse().toString());
+				return;
+			}
+			else if (onHistory) {
+				return;
+			}
+			
+			history = new  String[0];
+			addNewAction(2, new StringBuilder(forDelete).reverse().toString());
+			
 		}
 		
 	}
@@ -70,6 +100,63 @@ public class Level1
 	}
 	
 	private static void undo() {
+		if (historyIndex < -1 || historyIndex > history.length - 1) return;
+		String historyFragment = history[historyIndex];
 		
+		String numOperation = new String(new char[] {historyFragment.charAt(0)});
+		
+		String word = historyFragment.replaceFirst(numOperation + " ", "");
+		
+		switch (numOperation) {
+			case "1" : {
+				delete(word.length(), true);
+				--historyIndex;
+				break;
+			}
+			case "2" : {
+				add(word, true);
+				--historyIndex;
+				break;
+			}
+		}
+		
+	}
+	
+	
+	private static void redo() {
+		
+		if (historyIndex + 1 < -1 || historyIndex + 1 > history.length - 1) return;
+		
+		String historyFragment = history[historyIndex + 1];
+		
+		String numOperation = new String(new char[] {historyFragment.charAt(0)});
+		
+		String word = historyFragment.replaceFirst(numOperation + " ", "");
+		
+		switch (numOperation) {
+			case "1" : {
+//				++historyIndex;
+				add(word, true);
+				break;
+			}
+			case "2" : {
+//				++historyIndex;
+				delete(word.length(), true);
+				break;
+			}	
+		}
+	}
+	
+	
+	private static void addNewAction (int action, String word) {
+		String[] newHistory = new String[history.length + 1];
+		
+		for (int i = 0; i < history.length; ++i) {
+			newHistory[i] = history[i]; 
+		}
+		
+		newHistory[history.length] = action + " " + word;
+		historyIndex = history.length;
+		history = newHistory;
 	}
 }
